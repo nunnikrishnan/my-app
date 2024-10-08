aa6ad0d415703fc5e979e53bb67c97f8066d7457
nunnikrishnan <nunnikrishnan@altimetrik.com>
2024-10-08 15:37:26 +0530

    updated pom.xml and app.java files

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,14 @@
 package com.example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+      private static final Logger logger = LoggerFactory.getLogger(App.class); 
+
+      public static void main(String[] args) {
+        logger.info("Hello World!");
     }
 }
