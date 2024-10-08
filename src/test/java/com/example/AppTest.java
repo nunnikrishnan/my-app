1884c6e8def9ba0fabf292b51f8406c4c41ea150
nunnikrishnan <nunnikrishnan@altimetrik.com>
2024-10-08 14:08:41 +0530

    updated AppTest.java and pom.xml file

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -8,7 +8,13 @@ import org.junit.jupiter.api.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+    @Test
+    public void testMain() {
+        // Capture the output of the main method
+        String[] args = {};
+        App.main(args); // Call the main method
 
+    }
     /**
      * Rigorous Test :-)
      */
